3c49dcbc2069a8dc4742e6f7d00bdc652ac4b445
Huda-sh <huda.f.shakir@gmail.com>
2022-08-10 09:40:49 +0300

    a

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,12 @@
 import 'package:ecommerce/core/constant/routes.dart';
-import 'package:ecommerce/view/screen/auth/check_email.dart';
-import 'package:ecommerce/view/screen/auth/forget_password.dart';
+import 'package:ecommerce/view/screen/auth/signup/check_email.dart';
+import 'package:ecommerce/view/screen/auth/Forget_Password/forget_password.dart';
 import 'package:ecommerce/view/screen/auth/login.dart';
-import 'package:ecommerce/view/screen/auth/reset_password.dart';
-import 'package:ecommerce/view/screen/auth/signup.dart';
-import 'package:ecommerce/view/screen/auth/success_reset_password.dart';
-import 'package:ecommerce/view/screen/auth/success_signup.dart';
-import 'package:ecommerce/view/screen/auth/verify_code.dart';
+import 'package:ecommerce/view/screen/auth/Forget_Password/reset_password.dart';
+import 'package:ecommerce/view/screen/auth/signup/signup.dart';
+import 'package:ecommerce/view/screen/auth/Forget_Password/success_reset_password.dart';
+import 'package:ecommerce/view/screen/auth/signup/success_signup.dart';
+import 'package:ecommerce/view/screen/auth/Forget_Password/verify_code.dart';
 import 'package:ecommerce/view/screen/onboarding.dart';
 import 'package:flutter/material.dart';
 
